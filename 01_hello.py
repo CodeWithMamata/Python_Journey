398a6b518e092f5d9160fcf12ed116869fbfc9b5
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-07-04 00:30:41 +0530

    my first commit

---
+++ 01_hello.py
@@ -0,0 +1,48 @@
+# Day-1: Hello World
+
+print("Hello world")
+print("Hello Python")
+print("Apple,Mango,Banana")
+
+
+# \n is used to change next line (escape char at next line)
+
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print("Down came the rain\nand washed the spider out")
+
+print("My name is", "Python.")
+student_name = "Mamata"
+print("Welcome to course,", student_name)
+
+# end="" ka used tab hota hai jab alg alg lines ko ek hi line mai add krna ho
+
+print("My name is ", end="")
+print("Monty Python.")
+
+print("My", "name", "is", "Mamata", "mourya.", sep="-")
+print("My", "name", "is", sep="_", end="*")
+print("Mamata", "mourya.", sep="*", end="*\n")
+print("Hello")
+
+''' sample question is : print("Programming","Essentials","in")
+expected output is : Programming***Essentials***in...Python 
+Solve this problem: '''
+
+print("Programming", "Essentials", "in", sep="***", end="...")
+print("Python")
+
+
+
+
+print("       * ")
+print("      *  * ")
+print("     *    * ")
+print("    *      * ")
+print("   *        * ")
+print("   ***    ***")
+print("     *    * ")
+print("     *    * ")
+print("     *    * ")
+print("     *    * ")
+print("     *    * ")
+print("     ****** ")
---
+++ README.md
@@ -0,0 +1,22 @@
+ # 🐍 My Python Learning Journey
+
+Hi there 👋 I'm currently learning Python and building hands-on projects to strengthen my skills and grow as a developer. This repository is part of my practice and showcases my progress step by step.
+
+## 📚 What I'm Doing
+
+* Practicing Python basics (functions, loops, data types, etc.)
+* Solving Python code of practice and logic building, Data structures, and advanced Python concepts.
+* Building mini-projects. 
+
+## 🎯 Goals
+
+* Understand real-world applications of Python
+* Explore areas like Data Analytics, Artificial Intelligence and Machine Learning to build   advanced models.
+* Solve real-world problems.
+
+ ## 🛠️ Tech Stack & Tools
+ 
+* Language: Python
+* Tools: VS Code, Git, GitHub
+
+ 
