508ccea5010a94e6c99caf02f62998070ee99320
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-07-06 23:54:29 +0530

    added Python_basics

---
+++ python_basics.py
@@ -0,0 +1,26 @@
+
+# 1. Literals (Integer, Float, Octal, Hex) 
+
+int_val = 10 
+float_val = 2.5 
+oct_val = 0o123
+hex_val = 0x123 
+scientific_val = 3E8 
+
+
+# 2. Boolean values 
+
+# True = 1
+# False = 0
+print(True > False)
+print(True < False)
+
+# 3. Printing results to see outputs
+
+print("Integer :", int_val)
+print("float :", float_val)
+print("octal :", oct_val)
+print("hexa :", hex_val)
+print("scientific :", scientific_val)
+
+
