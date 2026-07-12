a5aeed9bfb07f7b43fc4df567dacca16c899f741
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-07-12 23:48:29 +0530

    added variable and data types

---
+++ 02_Fundamentals.py
@@ -0,0 +1,27 @@
+"""
+File: 02_fundamentals.py
+
+Topic Covered:
+- Variables
+- Data Types (int, float, str, bool)
+- Basics Operators 
+
+
+"""
+
+# 1. Variables And Data Types
+
+a = "Mamata mourya"
+print(a)
+a1 = 129
+a2 = 9
+a3 = 1.9
+print(a1 + a2)
+b = True
+c = None
+
+print("Type of a is", type(a))
+print("Type of a1 is", type(a1))
+print("Type of b is", type(b))
+print("Type of c is", type(c))
+print("Type of a3 is ", type(a3))
