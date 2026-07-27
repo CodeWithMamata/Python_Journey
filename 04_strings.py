904df69bb2dd7ee3ddd01ebc3db2e9af1c2e616a
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-07-27 22:38:55 +0530

    String

---
+++ 04_strings.py
@@ -0,0 +1,24 @@
+# Topic: String
+
+name = "Mamata"
+friend = "annu"
+anotherFriend = "Harshdha"
+
+print("Hello , " + name )
+print("HEllo , " + friend)
+print("Hello , " + anotherFriend)
+
+apple = ''' He said, 
+hii harry 
+hey I am good
+"I want to eat an apple"'''
+print(apple)
+print(name[0])
+print(friend[0])
+
+print("lets use a for loop\n")
+for chr in name:
+    print(chr)
+
+for type in name:
+    print(type)    
