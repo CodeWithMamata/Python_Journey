bdbaba2cc43c63fa4e7043c5a613c0e562f653e8
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-08-15 22:56:05 +0530

    if-else

---
+++ 05_conditions.py
@@ -0,0 +1,30 @@
+""" 
+05_Condition
+Topic covered:
+- Making decisions in Python 
+- If statements
+- Elif and Else statements
+- Conditional 
+
+"""
+
+
+
+# a = int(input("Enter your age:"))
+# print("Your age is:", a)
+
+# conditional operators
+#  <, >, <=, >=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a>=18)
+# print(a==18)
+# print(a!=18)
+
+bananaPrice = 50
+budget = 30
+
+if(bananaPrice <= budget):
+    print("Alexa, add 1 Dozen Bananas to the cart.")
+else:
+    print("Alexa, do not add bananas to the cart.")    
