aee168de3c2ccca5015b1b0b7ede14900f720119
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-08-18 22:07:01 +0530

    if-else statements

--- 05_conditions.py
+++ 05_conditions.py
@@ -21,6 +21,8 @@ Topic covered:
 # print(a==18)
 # print(a!=18)
 
+# if-else and elif conditions
+
 bananaPrice = 50
 budget = 30
 
@@ -28,3 +30,14 @@ if(bananaPrice <= budget):
     print("Alexa, add 1 Dozen Bananas to the cart.")
 else:
     print("Alexa, do not add bananas to the cart.")    
+
+num = int(input("Enter the value of num: "))
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is Zero.") 
+else:
+    print("Number is Positive.")   
+
+
+
