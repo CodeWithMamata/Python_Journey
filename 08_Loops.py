d5d3e937444b30bd00cb00baaece53b50821f065
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-08-21 23:04:07 +0530

    For Loop

---
+++ 08_Loops.py
@@ -0,0 +1,15 @@
+# LOOPS 
+
+#1.For Loops
+
+name = "Mamata"
+for i in name:
+    print(i)
+    if(i == "t"):
+        print("This is something special")
+
+colors = ["Red","Yellow","Pink","Blue","Marron"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
