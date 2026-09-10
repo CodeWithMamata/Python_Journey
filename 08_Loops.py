fd64195f4ff33e7c393879d8e29b53ab915070e8
Mamata mourya <mamata.mourya.cse@ghrcemj.raisoni.net>
2026-09-10 22:20:21 +0530

    update

--- 08_Loops.py
+++ 08_Loops.py
@@ -50,14 +50,11 @@ for i in range(1, 2):
 
 #2.While Loop
 
-i = 0
+'''  i = 0
 while(i<5):
     print(i)
-    i += 1
+    i += 1'''
+    
 
-count = 7
-while(count > 0):
-    print(count)
-    count = count - 1
-else:
-    print("I am inside else")
+
+
